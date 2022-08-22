12d9beb676d6a44731ba232f8127d5c5966e377a
Matheus Manhães <momanhaes@gmail.com>
2022-08-22 10:57:50 -0300

    feat: header hover

--- src/app/fragments/header/header.component.ts
+++ src/app/fragments/header/header.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public class!: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.class = 'fa fa-sticky-note fa-flip-horizontal mr-2';
   }
 
+  public mouseEnter() {
+    this.class = 'fa fa-sticky-note-o fa-flip-horizontal mr-2';
+  }
+
+  public mouseLeave() {
+    this.class = 'fa fa-sticky-note fa-flip-horizontal mr-2';
+  }
 }
